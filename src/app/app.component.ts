1f66506cfb0a6b2a05699b5d23f00bc46a1f9c6c
gaboisleno <isleno.gabriel@gmail.com>
2023-02-26 20:12:45 -0300

    translations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'cvpage';
+
+export class AppComponent implements OnInit {
+  title = 'Gabriel isleno resume';
+  age?:number;
+  language?:string;
+
+  constructor(private translate: TranslateService) {
+    translate.setDefaultLang('es');
+    translate.use('es');
+}
+
+  ngOnInit(): void {
+    const today = moment();
+    const bday = moment('1994-30-07', 'YYYY-DD-MM');
+
+    this.age = today.diff(bday, 'years');
+  }
+
+  toggleTranslation() {
+    if (this.translate.currentLang == 'es') {
+      this.translate.use('en');
+    }
+    else {
+      this.translate.use('es');
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import {TranslateModule, TranslateLoader} from '@ngx-translate/core';
+import {TranslateHttpLoader} from '@ngx-translate/http-loader';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+
+export function createTranslateLoader(http: HttpClient) {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: (createTranslateLoader),
+        deps: [HttpClient]
+    }
+  })
   ],
   providers: [],
   bootstrap: [AppComponent]
